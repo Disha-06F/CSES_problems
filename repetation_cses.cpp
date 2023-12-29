2c0c9dabf3560d6504330b4ea3435038b4d02900
Hasna Hena Joti <disha.amin025@gmail.com>
2023-12-30 03:54:53 +0600

    repetation problem cses

---
+++ repetation_cses.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int cur=1,res=1;
+    for(int i=1;i<s.length();i++){
+       if(s[i]==s[i-1]){
+        cur++;
+       }
+       else{
+        cur=1;
+       }
+       res=max(res,cur);
+
+    }
+    cout<<res<<"\n";
+      }
